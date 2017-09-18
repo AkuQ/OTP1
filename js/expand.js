eba01602890598b21d4756765dca87759570dd77
Janne <janneheikkila93@gmail.com>
2017-09-18 20:13:46 +0300

    Added chat.js that includes ajax and sendChat functions.

--- js/expand.js
+++ js/expand.js
@@ -9,3 +9,11 @@ function closeDesk() {
     document.getElementById("leftContent").className = "col-xs-12 col-sm-8";
     document.getElementById("rightContent").className = "col-xs-0 col-sm-4";
 }
+
+function openMenu() {
+    document.getElementById("dropMenu").style.display = "inline-block";
+}
+
+function closeMenu() {
+    document.getElementById("dropMenu").style.display = "none";
+}
