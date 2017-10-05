e4ebb92aa8b73b352857872697ca3513cf13c5ac
Tuomas Koivisto <tuomas.h.koivisto@metropolia.fi>
2017-10-05 13:15:16 +0300

    modaaliin huoneiden listaus, salasanakenttä huoneen luontivalikkoon

--- js/chat-area.js
+++ js/chat-area.js
@@ -2,4 +2,11 @@ $(function() {
     $(".scrollable").niceScroll();
 });
 
+
 $('#group-users').hide();
+
+$(".scrollable").mouseover(function() {
+    $(this).getNiceScroll().resize();
+    $(this).delay(500).getNiceScroll().show();
+            
+}); 
--- js/expand.js
+++ js/expand.js
@@ -2,19 +2,18 @@
 function openDesk() {
     document.getElementById("leftContent").className = "col-xs-0 col-sm-4";
     document.getElementById("rightContent").className = "col-xs-12 col-sm-8";
-    console.log("Chat window minimized");
+   //scrollbarToggle();
 }
 
 function closeDesk() {
     document.getElementById("leftContent").className = "col-xs-12 col-sm-8";
     document.getElementById("rightContent").className = "col-xs-0 col-sm-4";
-    console.log("Chat window maximized");
+   // scrollbarToggle();
 }
-
-function openMenu() {
-    document.getElementById("dropMenu").style.display = "inline-block";
-}
-
-function closeMenu() {
-    document.getElementById("dropMenu").style.display = "none";
-}
+/*
+function scrollbarToggle() {
+    $('.scrollable').getNiceScroll().hide();
+    $('.scrollable').delay(1000);
+    $('.scrollable').getNiceScroll().resize();
+    $('.scrollable').getNiceScroll().show();
+}  */
